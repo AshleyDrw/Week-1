29036435f9b2a6b10fefcbba79e6480df7912bbd
AshleyDrw <ashdrew1908@gmail.com>
2020-11-21 03:00:22 -0800

    added variables and assigned data types

---
+++ index.js
@@ -0,0 +1,60 @@
+let priceOfHat = 10;
+let priceOfShirt = 25;
+
+let moneyInAshleyWallet = 100;
+let moneyInJanisWallet = 50;
+
+let ashleyNumberOfFriends = 15;
+let janisNumberOfFriends = 30;
+
+let ashleyAgeInYears = 30;
+let janisAgeInYears = 28;
+
+let firstName = "Ashley";
+let firstName2 = "Janis";
+
+let lastName = "Drew";
+let lastName2 = "Colen";
+
+let middleInitial = "L";
+let middleInitial2 = "C";
+
+let ashleyNewWalletBalance = moneyInAshleyWallet - priceOfHat;
+let janisNewWalletBalance = moneyInJanisWallet - priceOfShirt;
+
+let ashleyNewFriendsPerYear = ashleyNumberOfFriends / ashleyAgeInYears;
+let janisNewFriendsPerYear = janisNumberOfFriends / janisAgeInYears;
+
+let fullName = firstName + " " + middleInitial + " " + lastName;
+let fullName2 = firstName2 + " " + middleInitial2 + " " + lastName2;
+
+console.log("The price of the new hat is:" + " " + priceOfHat);
+console.log("The price of the new shirt is:" + " " + priceOfShirt);
+
+console.log("The amount of money Ashley has in her wallet is:" + " " + moneyInAshleyWallet);
+console.log("The amount of money Janis has in her wallet is:" + " " + moneyInJanisWallet);
+
+console.log("The number of friends Ashley has is:" + " " + ashleyNumberOfFriends);
+console.log("The number of friends Janis has is:" +" " + janisNumberOfFriends);
+
+console.log("Ashley's age is:" + " " + ashleyAgeInYears);
+console.log("Janis's age is:" + " " + janisAgeInYears);
+
+console.log("User one's first name is:" + " " + firstName);
+console.log("User two's first name is:" + " " + firstName2);
+
+console.log("User one's middle initial is:" + " " + middleInitial);
+console.log("User two's middle initial is:" + " " + middleInitial2);
+
+console.log("User one's last name is:" + " " + lastName);
+console.log("User two's last name is:" + " " + lastName2);
+
+console.log("Ashley's wallet balance after purchasing a new hat is:" + " " + ashleyNewWalletBalance);
+console.log("Janis's wallet balance after purchasing a new shirt is:" + " " + janisNewWalletBalance);
+
+console.log("The number of new friends Ashley makes each year is:" + " " + ashleyNewFriendsPerYear);
+console.log ("The number of new friends Janis makes each year is:" + " " + janisNewFriendsPerYear);
+
+console.log("Ashley's full name is:" + " " + fullName);
+console.log("Janis's full name is:" + " " + fullName2);
+
